f9fb6ec39a90488abfaabc6887149d78dbca2449
William Jefferies <williamjefferies@Williams-MacBook-Pro.local>
2020-11-21 15:49:41 +1100

    schema and resolvers

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
+
+import schema from "./schema";
+import resolvers from "./resolvers";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+
+const root = resolvers;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.listen(8080, () => {
   console.log("Running on server port localhost:8080/graphql");
